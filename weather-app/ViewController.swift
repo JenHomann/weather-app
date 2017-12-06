438c541c3e292abb113a5eac178f785ae56a98a9
Jen Homann <jenhomann@Jens-MBP.igs2.com>
2017-12-06 14:24:56 -0600

    working weather app

--- weather-app/ViewController.swift
+++ weather-app/ViewController.swift
@@ -10,6 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var cityTextField: UITextField!
+    @IBOutlet var weatherLabel: UILabel!
+    @IBAction func submitButton(_ sender: Any) {
+        let city = cityTextField.text?.replacingOccurrences(of: " ", with: "-")
+        
+        if let url = URL(string: "http://www.weather-forecast.com/locations/\(city ?? "")/forecasts/latest") {
+            let request = NSMutableURLRequest(url: url)
+            
+            let task = URLSession.shared.dataTask(with: request as URLRequest) {
+                data, response, error in
+                
+                var message = ""
+                
+                if error != nil {
+                    print(error!)
+                } else {
+                    if let unwrappedData = data {
+                        let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                        var stringSeparator = "<span class=\"phrase\">"
+                        
+                        if let contentArray = dataString?.components(separatedBy: stringSeparator) {
+                            if contentArray.count > 1 {
+                                stringSeparator = "</span>"
+                                
+                                let newContentArray = contentArray[1].components(separatedBy: stringSeparator)
+                                if newContentArray.count > 1 {
+                                    message = newContentArray[0].replacingOccurrences(of: "&deg;", with: "°")
+                                }
+                            }
+                        }
+                    }
+                }
+                if message == "" {
+                    message = "The weather could not be found for your selection. Please try again."
+                }
+                
+                DispatchQueue.main.sync(execute: {
+                    self.weatherLabel.text = message
+                })
+            }
+            task.resume()
+        } else {
+            weatherLabel.text = "The weather could not be found for your selection. Please try again."
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +65,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        cityTextField.resignFirstResponder()
+        return true
+    }
 }
 
