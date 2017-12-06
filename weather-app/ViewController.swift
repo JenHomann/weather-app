61931333e62ceae63f3e316e0dec70e10f6f60f2
Jen Homann <jenhomann@Jens-MBP.igs2.com>
2017-12-06 11:43:21 -0600

    Initial Commit

---
+++ weather-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weather-app
+//
+//  Created by Jen Homann on 12/6/17.
+//  Copyright © 2017 Jen Homann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
